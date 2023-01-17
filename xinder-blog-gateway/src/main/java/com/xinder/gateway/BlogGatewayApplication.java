6987f1353fd709c11e98782a4d77ed874cf5965f
dong <>
2023-01-17 20:30:30 +0800

    修改打包依赖

--- xinder-blog-gateway/src/main/java/com/xinder/gateway/BlogGatewayApplication.java
+++ xinder-blog-gateway/src/main/java/com/xinder/gateway/BlogGatewayApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @date 2023-01-12 21:24
  */
 @SpringBootApplication(exclude = RedisAutoConfiguration.class)
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class BlogGatewayApplication {
 
     public static void main(String[] args) {
